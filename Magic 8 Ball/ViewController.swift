23a1a93ff8ccc76695567e71b50593ae0e157a3b
Stanley Liao <aegis2469111@gmail.com>
2017-09-29 11:00:29 +0800

    Add constraints

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber : Int = 0
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        randomBallNumber = Int(arc4random_uniform(4))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
     }
-
-
+    
+    func newBallImage() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
 }
 
